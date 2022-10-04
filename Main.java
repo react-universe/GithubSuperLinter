c33ba2b166c32b6bc99476899c4b212e18ec3d31
react-universe <react.incipientinfo@gmail.com>
2022-10-04 22:07:29 +0530

    forth try

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c, largest;
+        // object of the Scanner class
+        Scanner sc = new Scanner(System.in);
+        // reading input from the user
+        Systm.ot.prntln("Enter the first number:");
+        a = sc.nextInt()
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
+        largest = c > (a > b ? a : b) ? c : (a > b) ? a : b);
+        System.ut.pritln("The largest number is: " + largest);
+    }
+}
